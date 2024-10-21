9f5c744650cf784b48e3b1741b1936d801f8efe5
evgenijab <evgenija.butleska@gmail.com>
2024-10-21 10:56:51 +0200

    local json acf setup

--- simple-theme/functions.php
+++ simple-theme/functions.php
@@ -176,6 +176,37 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+add_filter( 'acf/settings/save_json', 'my_acf_json_save_point' );
 /**
- * testing commit.
+ * Save ACF fields to json file.
+ *
+ * @param array $path returns file path.
+ * @return array
  */
+function my_acf_json_save_point( $path ) {
+
+	// update path.
+	$path = get_template_directory() . '/acf-json';
+
+	// return.
+	return $path;
+}
+
+add_filter( 'acf/settings/load_json', 'my_acf_json_load_point' );
+/**
+ * Load acf json file.
+ *
+ * @param array $paths return the path.
+ * @return array
+ */
+function my_acf_json_load_point( $paths ) {
+
+	// remove original path (optional).
+	unset( $paths[0] );
+
+	// append path.
+	$paths[] = get_template_directory() . '/acf-json';
+
+	// return.
+	return $paths;
+}
