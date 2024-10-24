bfb3c1df1cb20d8e695211f4929e236ac9deb033
evgenijab <evgenija.butleska@gmail.com>
2024-10-24 21:35:18 +0200

    install bootstrap

--- simple-theme/functions.php
+++ simple-theme/functions.php
@@ -142,7 +142,7 @@ function simple_theme_scripts() {
 	wp_style_add_data( 'simple-theme-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'simple-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
+	wp_enqueue_style('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
